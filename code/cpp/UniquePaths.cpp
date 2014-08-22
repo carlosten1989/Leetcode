852f35712acd26f870b946ef11368f8112277ece
carlosten1989 <lj007john@163.com>
2014-08-22 23:04:05 +0800

    uniquepath

--- code/cpp/UniquePaths.cpp
+++ code/cpp/UniquePaths.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        if(m == 0 || n == 0)
-        {
+        if(m == 0 && n == 0) {
             return 0;
         }
         int i,j;
         int dp[m][n];
-        for(i = 0;i < m;i ++)
-        {
-            for(j = 0;j < n;j ++)
-            {
-                if(i == 0 && j == 0)
-                {
+        for(i = 0;i < m;i ++) {
+            for(j = 0;j < n;j ++) {
+                if(i == 0 && j == 0) {
                     dp[i][j] = 1;
-                }
-                else if(i == 0)
-                {
+                } else if(i == 0) {
                     dp[i][j] = dp[i][j-1];
-                }
-                else if(j == 0)
-                {
+                } else if(j == 0) {
                     dp[i][j] = dp[i-1][j];
-                }
-                else
-                {
-                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
                 }
             }
         }
