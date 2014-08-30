52e070710c15e6c342781b5b800f6dda44ab2c6a
carlosten1989 <lj007john@163.com>
2014-08-31 00:38:05 +0800

    fix bug

--- code/cpp/ScrambleString.cpp
+++ code/cpp/ScrambleString.cpp
@@ -3,42 +3,27 @@ public:
     bool isScramble(string s1, string s2) {
         int len1 = s1.length();
         int len2 = s2.length();
-        if(len1 != len2)
-        {
+        if(len1 != len2) {
             return false;
         }
-        if(len1 == 0)
-        {
+        if(len1 == 0) {
             return true;
         }
-        bool f[len1][len1][len1];
+        int len = len1;
+        bool f[len][len][len];
         int i,j,k;
-        for(i = 0;i < len1;i ++)
-        {
-            for(j = 0;j < len1;j ++)
-            {
-                if(s1[i] == s2[j])
-                {
-                    f[0][i][j] = true;
-                }
-                else
-                {
-                    f[0][i][j] = false;
-                }
+        for(i = 0;i < len;i ++) {
+            for(j = 0;j < len;j ++) {
+                f[0][i][j] = s1[i] == s2[j];
             }
         }
-        for(k = 1;k < len1;k ++)
-        {
-            for(i = 0;i < len1 - k;i ++)
-            {
-                for(j = 0;j < len1 - k;j ++)
-                {
+        for(k = 1;k < len;k ++) {
+            for(i = 0;i < len-k;i ++) {
+                for(j = 0;j < len-k;j ++) {
                     int m;
                     bool tmp = false;
-                    for(m = 0;m < k;m ++)
-                    {
-                        if((f[m][i][j] && f[k-1-m][i+m+1][j+m+1]) || (f[m][i][j+k-m] && f[k-1-m][i+m+1][j]) )
-                        {
+                    for(m = 0;m < k;m ++) {
+                        if((f[m][i][j] && f[k-1-m][i+m+1][j+m+1]) || (f[m][i][j+k-m] && f[k-1-m][i+m+1][j])) {
                             tmp = true;
                             break;
                         }
@@ -47,6 +32,6 @@ public:
                 }
             }
         }
-        return f[len1-1][0][0];
+        return f[len-1][0][0];
     }
 };
